3195466720fec4f0ca40de15e198b3ae4a70f368
Bruno <bcoronado.morales@gmail.com>
2020-04-03 23:23:10 -0600

    aws db config

--- config/database.js
+++ config/database.js
@@ -3,6 +3,7 @@ module.exports = {
         host: process.env.RDS_HOSTNAME,
         user: process.env.RDS_USERNAME,
         password: process.env.RDS_PASSWORD,
-        port: process.env.RDS_PORT
+        port: process.env.RDS_PORT,
+        database: "ebdb"
     }
 }
