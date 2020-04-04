c2df8e5269427c5ee3e74c4449d331ed0ed71112
Bruno <bcoronado.morales@gmail.com>
2020-04-03 23:54:54 -0600

    aws rds final config

--- config/database.js
+++ config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
     pi_connection: {
-        host: 'aa1qiu80aq6mmqn.cfrhwnzwaldz.us-east-2.rds.amazonaws.com',//process.env.RDS_HOSTNAME,
-        user: 'BrunoCoronado',//process.env.RDS_USERNAME,
-        password: '23111997',//process.env.RDS_PASSWORD,
-        port: 3306,//process.env.RDS_PORT,
-        database: "ebdb"//RDS_DB_NAME
+        host: process.env.RDS_HOSTNAME,
+        user: process.env.RDS_USERNAME,
+        password: process.env.RDS_PASSWORD,
+        port: process.env.RDS_PORT,
+        database: process.env.RDS_DB_NAME
     }
 }
