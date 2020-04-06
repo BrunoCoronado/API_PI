40f5f208ab4b9fcd3fae1890e9e7f5f877206b7a
Bruno <bcoronado.morales@gmail.com>
2020-04-06 15:21:15 -0600

    Venta y Orden Transferencia

---
+++ controllers/venta.js
@@ -0,0 +1,38 @@
+const venta = require('../db_apis/venta');
+
+async function get(request, response, next){
+    try{
+        response.status(200).json("");
+    }catch(error){
+        next(error);
+    }
+}
+
+async function post(request, response, next){
+    try{
+        response.status(200).json("");
+    }catch(error){
+        next(error);
+    }
+}
+
+async function put(request, response, next){
+    try{
+        response.status(200).json("");
+    }catch(error){
+        next(error);
+    }
+}
+
+async function del(request, response, next){
+    try{
+        response.status(200).json("");
+    }catch(error){
+        next(error);
+    }
+}
+
+module.exports.get = get;
+module.exports.post = post;
+module.exports.put = put;
+module.exports.delete = del;
--- services/router.js
+++ services/router.js
@@ -2,8 +2,23 @@ const express = require('express');
 const router = new express.Router();
 
 const departamento = require('../controllers/departamento');
+const venta = require('../controllers/venta');
+const ordenTransferencia = require('../controllers/ordenTransferencia');
+
 
 router.route('/departamento')
     .get(departamento.get);
+
+router.route('/venta/:codigo?')
+    .get(venta.get)
+    .post(venta.post)
+    .put(venta.put)
+    .delete(venta.delete);
+
+router.route('/ordenTransferencia/:codigo?')
+    .get(ordenTransferencia.get)
+    .post(ordenTransferencia.post)
+    .put(ordenTransferencia.put)
+    .delete(ordenTransferencia.delete);
 
 module.exports = router;
